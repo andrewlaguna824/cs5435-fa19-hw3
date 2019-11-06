48399daa0a4eb4ba4d79b25a5e0d457ef4c5df64
Andrew Palmer <ajp294@cornell.edu>
2019-11-06 10:59:05 -0500

    poattack update with pseudo code. test poattack code

--- poattack.py
+++ poattack.py
@@ -1,6 +1,8 @@
 import os
 from cryptography.hazmat.primitives import hashes, padding, ciphers
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import algorithms
+
 from requests import codes, Session
 import requests
 
@@ -24,6 +26,7 @@ class PaddingOracle(object):
     def __init__(self, po_url): 
         self.url = po_url
         self._block_size_bytes = ciphers.algorithms.AES.block_size/8
+        print("Block size bytes: {}".format(self._block_size_bytes))
 
     @property
     def block_length(self):
@@ -48,18 +51,28 @@ def po_attack_2blocks(po, ctx):
     """
     assert len(ctx) == 2*po.block_length, "This function only accepts 2 block "\
         "cipher texts. Got {} block(s)!".format(len(ctx)/po.block_length)
-    c0, c1 = list(split_into_blocks(ctx, po.block_length))
-    msg = ''
+    c0, c1 = list(split_into_blocks(ctx, int(po.block_length)))
+    print("C0 (len {}): {}".format(len(c0), c0))
+    print("C1 (len {}): {}".format(len(c1), c1))
+
+    # Try just to get one padding correct
+    # test = C0 ^ 0x01 ^ 0x0i | C1 ?
+    # eest = c0[15] ^ 1 ^ 1
+    # print("test string: {}".format(str(test).encode('utf-8')))
+    # check_padding_response(str(test).encode('utf-8'))
+
+
     # TODO: Implement padding oracle attack for 2 blocks of messages.
 
     plaintext = bytearray(16)
-    for i in range(15, -1, -1):
-        test = bytearray(16)
+    for i in range(15, -1, -1): 
+        test = bytearray(16) 
         for val in range(256):
             test[i] = bytes([val])
             print(test)
             # if test passes padding check then
-            # plaintext[i] = val ^ c0[i] ^ bytes([i])
+            if (check_padding_response(str(test).encode('utf-8')):
+                plaintext[i] = val ^ c0[i] ^ bytes([i]) # Can't XOR bytes objects
     return msg
 
 def po_attack(po, ctx):
@@ -68,7 +81,7 @@ def po_attack(po, ctx):
     @po: an instance of padding oracle. 
     You don't have to unpad the message.
     """
-    ctx_blocks = list(split_into_blocks(ctx, po.block_length))
+    ctx_blocks = list(split_into_blocks(ctx, po.block_length)) # TODO: Is this block length correct for the hex string?
     nblocks = len(ctx_blocks)
     # TODO: Implement padding oracle attack for arbitrary length message.
 
@@ -113,8 +126,20 @@ if __name__ == "__main__":
     print("CO: {}".format(C0))
     admin_cookie_bytes[0] = 1
 
-    success = check_padding_response(bytes(16))
-    print("Success? {}".format(success))
+    # Check padding response success
+    # success = check_padding_response(bytes(16))
+    # print("Success? {}".format(success))
+
+    TEST_COOKIE = bytearray.fromhex("e9fae094f9c779893e11833691b6a0cd3a161457fa8090a7a789054547195e606035577aaa2c57ddc937af6fa82c013d")
+    print("Test cookie (len {}): {}".format(len(TEST_COOKIE), TEST_COOKIE))
+    # success = check_padding_response(TEST_COOKIE)
+
+    print("test cookie first 32 bytes: {}".format(TEST_COOKIE[:32]))
+    first_two_blocks = TEST_COOKIE[:32]
+    # Instantitate padding oracle
+    po = PaddingOracle(SETCOINS_FORM_URL)
+    po_attack_2blocks(po, first_two_blocks)
+
 
     # TODO: Maul code from part 1.1
     # Maul the admin cookie in the 'sess' object here
